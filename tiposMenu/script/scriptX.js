1ed21af664b875d9b91b5eb42d7e1f84fed7dd1b
Adriano Ribeiro <94660112+AdrianoNRibeiro@users.noreply.github.com>
2022-05-09 23:07:01 -0300

    9-5.js

---
+++ tiposMenu/script/scriptX.js
@@ -0,0 +1,51 @@
+//Meu primeiro programa
+//Criador: Adriano
+/*
+primeiro
+programa*/
+/*
+var a = 2;
+var b = 3;
+var c = "Ola"
+
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+
+if (c%2 == 1){
+    alert("Número ímpar!")
+}
+else if (c%2 == 0){
+    alert("Número par!")
+}
+else {
+    alert("Número Inválido!")
+}
+
+var i = 0;
+
+while (i<3){
+    alert(i);
+
+    i += 1;
+}
+
+
+for (var j = 0; j < 3; j++){
+    alert(j);
+}
+
+var numero = 6;
+var decimal = 4.5;
+var string = "Olá Mundo";
+var lista = ["laranja", "maçã", "banana", 1234];
+
+//alert(lista[2]);
+
+for (i in lista) {
+    alert(lista[i]);
+}*/
+
+console.log("Olá Mundo!");
